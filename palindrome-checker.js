8e2afc63366e83f4821237e4082821d06d43ac6a
Jonathasdp <jonathas.dp@gmail.com>
2021-09-19 20:55:35 -0300

    Remoção de código desnecessário.

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,6 +1,5 @@
 function palindrome(str) {
   function isPalindrome(value) {
-
     // Primeiro armazena em uma variável o index que corresponde a metade do array.
     let half = Math.floor(value.length / 2);
 
@@ -12,10 +11,12 @@ function palindrome(str) {
     // Utiliza o método do splice para obter os dois caracteres centrais e os remove da string principal.
     let middle = value.splice(half - 1, 2);
 
-    // Verifica se os caracteres são iguais e, ou chegamos ao fim do array ou recursivamente 
+    // Verifica se os caracteres são iguais e, ou chegamos ao fim do array ou recursivamente
     // o array que sobrou é um palíndromo.
-    return middle[0] === middle[1] && (value.length == 0 || isPalindrome(value));
-  };
+    return (
+      middle[0] === middle[1] && (value.length == 0 || isPalindrome(value))
+    );
+  }
 
   // Transforma string em lower case e em seguida remove os caracteres não alfa númericos e símbolos da string.
   let result = str.toLowerCase().match(/[^\W_]/gi);
@@ -23,6 +24,3 @@ function palindrome(str) {
   // Retorna verdadeiro em caso do array ser um palíndromo.
   return isPalindrome(result);
 }
-
-
-console.log(palindrome("A man, a plan, a canal. Panama"));
